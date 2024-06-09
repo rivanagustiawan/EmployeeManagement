9567bb7a6e8a4bf78f729b07b794604bcf8060fe
Rivan Agustiawan <nawaitsuga17@gmail.com>
2024-06-09 15:29:32 +0700

    Add employee management database schema with models and migrations

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body class="relative bg-green-50 overflow-hidden max-h-screen">
+
+    {{-- SIDE BAR  --}}
+    <section class="fixed inset-y-0 left-0 bg-white shadow-md max-h-screen w-60">
+      <div class="flex flex-col justify-between h-full">
+        <div class="flex-grow">
+          <div class="px-4 py-6 text-center border-b">
+            <h1 class="text-xl font-bold leading-none"><span class="text-green-700">Kazee Test</span> App</h1>
+          </div>
+          <div class="p-4">
+            <ul class="space-y-1">
+              <li>
+                <a href="/" class="flex items-center bg-green-200 rounded-xl font-bold text-sm text-green-900 py-3 px-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-lg mr-4" viewBox="0 0 16 16">
+                    <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z"/>
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="p-4">
+          <button type="button" class="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="" viewBox="0 0 16 16">
+              <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+            </svg>
+          </button> <span class="font-bold text-sm ml-2">Logout</span>
+        </div>
+      </div>
+    </section>
+
+    {{-- END SIDEBAR  --}}
+  
+    <main class="ml-60 pt-16 max-h-screen overflow-auto">
+        @yield('dashboard')
+    </main>
+  </body>
+</html>
